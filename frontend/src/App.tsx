a99f8eefcb6a56bf3cb51c8ea19c50e71c9dcbc6
GowthamChowta <gautigadu091@gmail.com>
2023-08-06 12:08:31 -0500

    React + Redux + Prettier + Eslint setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is one stop to know whats happening in Bloomington</h1>
+    </div>
+  );
+}
+
+export default App;
